63c97f0120ea5f55706bcc1b584f0b9a62597071
MiScarn <bringmenoize@gmail.com>
2023-10-31 23:42:58 -0400

    testng repository

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,4 +1,44 @@
 package ru.netology.service;
 
+import static org.testng.Assert.assertEquals;
+
+
 public class CashbackHackServiceTest {
+
+    @org.testng.annotations.Test
+    public void remain600() {    //метод
+
+        CashbackHackService service = new CashbackHackService();
+        int amount = 600;
+
+        int actual = service.remain(amount);
+        int expected = 400;
+
+        assertEquals(actual, expected);
+    }
+
+    @org.testng.annotations.Test
+    public void remain999() {    //метод
+
+        CashbackHackService service = new CashbackHackService();
+        int amount = 999;
+
+        int actual = service.remain(amount);
+        int expected = 1;
+
+        assertEquals(actual, expected);
+    }
+
+    @org.testng.annotations.Test
+    public void remain1000() {    //метод
+
+        CashbackHackService service = new CashbackHackService();   // сумма покупки 1000 рублей, начисляется кэшбэк
+        int amount = 1000;
+
+        int actual = service.remain(amount);
+        int expected = 0;     //
+
+        assertEquals (actual, expected);
+    }
 }
+
